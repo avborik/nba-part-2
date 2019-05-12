029094c0c71d84e0fb70e694d69a92e6c9da56b7
AvBorikDeveloper <avboris28@gmail.com>
2019-05-12 17:01:21 +1200

    dasboard form

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ import VideoArticle from './components/Articles/Videos/Video/index';
 import NewsMain from './components/Articles/News/Main/index';
 import VideosMain from './components/Articles/Videos/Main/index';
 import SignIn from './components/signin/signin';
+import Dashboard from './components/Dashboard/dashboard';
 
 const Routes = (props)=>{
     
@@ -21,7 +22,8 @@ const Routes = (props)=>{
                     <Route path="/articles/:id" exact component={NewsArticle}/>
                     <Route path="/videos/:id" exact component={VideoArticle}/>
                     <Route path="/videos" exact component={VideosMain}/>
-                    <Route path="/sign-in" exact component={SignIn}/>               
+                    <Route path="/sign-in" exact component={SignIn}/>
+                    <Route path="/dashboard" exact component={Dashboard}/>             
                 </Switch>
             </Layout>   
         ) 
