15dcbf07f96b8c3a62aaba03522ac0f55bb17c11
AvBorikDeveloper <avboris28@gmail.com>
2019-05-14 21:25:45 +1200

    file uploading

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -5,6 +5,7 @@ import {Editor} from 'react-draft-wysiwyg';
 import {EditorState, convertFromRaw, convertToRaw} from 'draft-js';
 import {stateToHTML} from 'draft-js-export-html';
 import { firebaseTeams, firebaseArticles, firebase } from '../../firebase';
+import Uploader from '../widgets/FileUploader/fileUploader';
 
 class Dashboard extends Component {
     state={
@@ -46,6 +47,11 @@ class Dashboard extends Component {
             },
             body:{
                 element:'texteditor',
+                value:'',
+                valid:true
+            },
+            image:{
+                element:'image',
                 value:'',
                 valid:true
             },
@@ -188,11 +194,19 @@ class Dashboard extends Component {
         })
     }
 
+    storeFilename = (filename) => {
+        this.updateForm({id:'image'},filename)
+    }
+
     render(){
         return(
             <div className={styles.postContainer}>
                 <form onSubmit={this.submitForm}>
                     <h2>Add Post</h2>
+
+                    <Uploader
+                        filename={ (filename)=>this.storeFilename(filename)}
+                    />
                     
                     <FormField
                         id={'author'}
@@ -223,6 +237,7 @@ class Dashboard extends Component {
                     { this.showError() }
                   
                 </form>
+                
             </div>
         )
     }
